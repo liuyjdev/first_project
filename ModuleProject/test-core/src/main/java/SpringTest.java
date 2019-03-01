84bae5962d86286397e77a613a2b6b388648518a
liuyingjie <liuyingjie@cn.wilmar-intl.com>
2019-03-01 16:29:51 +0800

    first

---
+++ ModuleProject/test-core/src/main/java/SpringTest.java
@@ -0,0 +1,11 @@
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author liuyingjie
+ * @Description spring测试
+ * @create 2019-03-01 16:12
+ */
+@Controller
+public class SpringTest {
+
+}
