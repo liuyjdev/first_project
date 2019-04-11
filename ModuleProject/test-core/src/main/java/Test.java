802bcdf1faae7f8c79744b64aaa05bc59268be93
liuyj <liuyj_dev@aliyun.com>
2019-04-11 20:51:15 +0800

    提交

---
+++ ModuleProject/test-core/src/main/java/Test.java
@@ -0,0 +1,21 @@
+/**
+ * @Description: java类作用描述
+ * @Author: lyj
+ * @CreateDate: 2019/3/3$ 13:25$
+ * @Version: 1.0
+ */
+public class Test {
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public String getName(String id){
+        return null;
+    }
+
+    public String getNameByTemplate(String id, int age) throws ClassCastException{
+        return null;
+    }
+}
