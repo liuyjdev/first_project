f12fca16035e120444f04d716d3e63797a3660c8
liuyj <liuyj_dev@aliyun.com>
2019-04-11 20:51:35 +0800

    2

---
+++ ModuleProject/test-core/src/main/java/HashMapTest.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("name","lyj");
+        System.out.println(map);
+    }
+}
